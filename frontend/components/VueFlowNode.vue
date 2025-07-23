d088bb8ae1fd29f66977a4297a9b9801c0623e01
tatsuru koike <t-koike@marsflag.com>
2025-07-23 18:55:46 +0900

    feat: VueFlowNodeコンポーネントのTooltipを追加し、インターメディエイトノードのスタイルを改善

--- frontend/components/VueFlowNode.vue
+++ frontend/components/VueFlowNode.vue
@@ -18,7 +18,27 @@ const props = defineProps({
 </script>
 
 <template>
-  <Card class="node relative w-[250px] rounded-sm py-4">
+  <Card
+    class="node relative w-[250px] rounded-sm py-4"
+    :class="props.data.isIntermediate && 'bg-muted'"
+  >
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger
+          as-child
+          class="absolute top-2 right-2 z-10 flex size-fit cursor-pointer"
+        >
+          <Icon
+            v-if="props.data.isIntermediate"
+            name="mdi-close-circle"
+            class="text-destructive !size-4 shrink-0"
+          />
+        </TooltipTrigger>
+        <TooltipContent>
+          <p class="text-xs">このページは存在しません。</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
     <CardContent class="flex flex-col gap-1 px-4">
       <h3 class="text-xs">
         {{ props.data.title }}
@@ -26,7 +46,12 @@ const props = defineProps({
       <NuxtLink
         :to="props.data.url"
         target="_blank"
-        class="text-link text-xs break-words hover:underline"
+        class="text-xs break-words hover:underline"
+        :class="
+          props.data.isIntermediate
+            ? 'text-muted-foreground pointer-events-none'
+            : 'text-link'
+        "
       >
         {{ props.data.url }}
       </NuxtLink>
